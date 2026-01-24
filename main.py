df4244fe68a9d8c13dcf74daaea0bce062fb2951
gabicjo <olhosdegamer@gmail.com>
2026-01-24 18:17:21 -0300

    calcular vendas por filial

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ arquivo = caminho / 'supermarket_sales.csv'
 df = pd.read_csv(arquivo, sep=',')
 df['data'] = pd.to_datetime(df['data'])
 df = df.set_index('data')
+
+vendas_por_filial = df.groupby('filial')['total'].agg(['sum', "count"]).reset_index().sort_values('sum', ascending=False)
+vendas_por_filial['sum'] = round(vendas_por_filial['sum'], 2)
