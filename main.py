67ed5f8d750b26c61ac018b08a8b2bdeefa18a34
gabicjo <olhosdegamer@gmail.com>
2026-01-25 08:57:16 -0300

    adicionar ranking por categoria e filial

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import pandas as pd
 from pathlib import Path
 
-caminho = Path('dados')
-arquivo = caminho / 'supermarket_sales.csv'
-
-df = pd.read_csv(arquivo, sep=',')
-
 class Loja:
     def __init__(self, df: pd.DataFrame):
         self.df = df
     
     # * A filial C tem maior faturamento
-    # ! Sera que as avaliações delas são boas?
+    # ? Sera que as avaliações delas são boas?
+    # ! Sim, a filial c tem as melhoras avaliações
     def vendas_por_filial(self):
         vendas = self.df.groupby('filial')['total'].agg(['sum', "count"]).reset_index().sort_values('sum', ascending=False)
         vendas['sum'] = round(vendas['sum'], 2)
@@ -36,7 +32,7 @@ class Loja:
         return vendas
     
     # * as mulheres gastam mais que homens
-    # ! as mulheres costumam dar avaliações positivas?
+    # ? as mulheres costumam dar avaliações positivas?
     def vendas_por_genero(self):
         vendas = self.df.groupby("genero")['total'].agg(['sum', 'mean', 'count']).reset_index().sort_values('sum', ascending=False)
         vendas['sum'] = round(vendas['sum'], 2)
@@ -62,5 +58,23 @@ class Loja:
         self.df['hora'] = pd.to_datetime(self.df['hora'], format='%H:%M').dt.hour
         horarios = self.df.groupby('hora').size().reset_index(name='quantidade').sort_values('quantidade', ascending=False)
         return horarios
-    
+
+    def ranking_por_filial(self):
+        ranking = self.df.groupby('filial')['avaliacao'].mean().reset_index(name="ranking").sort_values('ranking', ascending=False)
+        ranking['ranking'] = round(ranking['ranking'], 2)
+        return ranking
+
+    def ranking_por_categoria(self):
+        ranking = self.df.groupby('categoria')['avaliacao'].mean().reset_index(name="ranking").sor_values('ranking', ascending=False)
+        ranking['ranking'] = round(ranking['ranking'], 2)
+
+        return ranking
+
+caminho = Path('dados')
+arquivo = caminho / 'supermarket_sales.csv'
+
+df = pd.read_csv(arquivo, sep=',')
+
 mercado = Loja(df)
+
+print(mercado.ranking_por_filial())
