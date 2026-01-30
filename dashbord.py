bb62a3ebcae84ee29ddcb49ac42eb4e4dcee2d97
gabicjo <olhosdegamer@gmail.com>
2026-01-30 19:00:05 -0300

    colocar grafico de metodos de pagamento + primeiros detalhes pa gamina inicial

--- dashbord.py
+++ dashbord.py
@@ -5,9 +5,17 @@ import plotly.express as px
 import locale
 
 # extrair dados
+nome = "Gabriel"
 
 # tratar aquilo que vem da interface
 st.set_page_config(layout='wide', page_title='Dashboard Supermercado')
 
 # construir a interface
-st.write(store.vendas_por_filial)
+st.write(f'## Olá, {nome} 👋')
+st.write(f"Veja as estatisticas do seu negocio e veja ele crescendo!")
+
+col1, col2 = st.columns([2, 1])
+
+ctn_metodos_pagamento = col2.container(border=True, horizontal_alignment='center')
+ctn_metodos_pagamento.markdown("<h4 style='text-align: center;'> Metodos de Pagamento</h4>", unsafe_allow_html=True)
+ctn_metodos_pagamento.plotly_chart(store.metodos_pagamento_mais_usados(), use_container_width=False)
