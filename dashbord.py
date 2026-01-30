65e9e8c573103717056b5e6570c3eeabd68167e1
gabicjo <olhosdegamer@gmail.com>
2026-01-30 18:58:33 -0300

    importar locale e configurar largura da pagina

--- dashbord.py
+++ dashbord.py
@@ -2,10 +2,12 @@ from main import store
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import locale
 
 # extrair dados
 
 # tratar aquilo que vem da interface
+st.set_page_config(layout='wide', page_title='Dashboard Supermercado')
 
 # construir a interface
 st.write(store.vendas_por_filial)
